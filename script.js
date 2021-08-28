aa9f9c9a436bf9f7cb7c1937a9aaf1555c11eac6
Shariq <shariq7991@gmail.com>
2021-08-28 15:21:07 +0100

    grid

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    // cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+// makeRows(2,2);
+// makeRows(16,16);
+makeRows(64,64);
+
+var blocks = document.querySelectorAll('.grid-item');
+blocks.forEach(block => block.addEventListener('mouseover', event =>{
+    block.setAttribute('style', 'background-color:black');
+}))
+
+
+
